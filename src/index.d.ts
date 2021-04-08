80ca24121db125a8c5b90ae6cee9890f20f2be6f
afeizt <afeizt@hotmail.com>
2021-04-08 22:32:41 +0800

    fix ts declare  file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,10 @@
-export as namespace mycmUtils;
+
 
 declare module 'mycm-utils'{
- export function cutHtml(str:string,length:number?):string
+ export function cutHtml(str:string):string;
+ export function cutHtml(str:string,length:number):string;
  export function dateFormat(timestamp:string|number, format:string):string;
- export function cutStr(str:string,length:number?):string;
+ export function cutStr(str:string):string;
+ export function cutStr(str:string,length:number):string;
  export function htmlToText(htmlSting:string):string
 }
